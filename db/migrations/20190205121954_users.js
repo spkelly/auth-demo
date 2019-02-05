b11717bafd4c994b440ea680698057485279c8d0
Sean Kelly <spkelly18@gmail.com>
2019-02-05 14:51:40 -0700

    added acount_type to user table migration

--- db/migrations/20190205121954_users.js
+++ db/migrations/20190205121954_users.js
@@ -1,14 +1,16 @@
 // migrate:make users --knexfile ./db/config.js
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.createTable('users',(table)=>{
-    table.increments().notNull().primary();
-    table.string('name').notNull();
+    table.increments('user_id').notNull().primary();
+    table.string('display_name').notNull();
+    table.string('account_type');
+    table.string('email').unique().notNull();
     table.string('hash').notNull();
     table.string('salt').notNull();
-    table.timeStamps();
+    table.timestamps();
   })
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.dropTable('users');
 };
