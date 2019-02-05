7ddeecc01f0e88ef692c697a234a546e536e968f
Sean Kelly <spkelly18@gmail.com>
2019-02-05 14:06:16 -0700

    added basic user table migration and changed pg database config

--- db/config.js
+++ db/config.js
@@ -1,6 +1,13 @@
+require('dotenv').config({path:'../.env'});
+
 module.exports = {
   development:{
     client: 'pg',
-    connection:'postgres://localhost/auth_dev'
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    }
   }
 }
--- db/migrations/20190205121954_users.js
+++ db/migrations/20190205121954_users.js
@@ -1,8 +1,14 @@
 // migrate:make users --knexfile ./db/config.js
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users',(table)=>{
+    table.increments().notNull().primary();
+    table.string('name').notNull();
+    table.string('hash').notNull();
+    table.string('salt').notNull();
+    table.timeStamps();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('users');
 };
