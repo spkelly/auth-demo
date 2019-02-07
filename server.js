0b373d386ecaf7df5f48ff9ed27c5016a48a7f8d
Sean Kelly <spkelly18@gmail.com>
2019-02-07 11:34:05 -0700

    added body parser and simple error handling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require('express');
 const routes = require('./routes');
 const requestLogger = require('./middleware').requestLogger;
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 
 const app = express();
@@ -11,10 +13,26 @@ app.set('view engine', 'pug');
 app.set('x-powered-by', false);
 app.set('views', path.resolve(__dirname,"views"));
 
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname,"public")));
-
 app.use(requestLogger);
 app.use(routes);
-
+app.use(errorLogging);
+app.use(errorHandler);
 app.listen(port);
 
+function errorLogging(err, req,res,next){
+  
+  if(process.env.NODE_ENV != 'production'){   
+    console.error(err);
+  }
+  next(err);
+};
+
+function errorHandler(err, req,res,next){
+  if(!err.statusCode){
+    err.status.Code = 500;
+  }
+
+  res.sendStatus(err.StatusCode).json(err);
+}
