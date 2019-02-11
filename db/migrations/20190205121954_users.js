dfba69b785624fc5b49d4e88c9203d8de1258836
Sean Kelly <spkelly18@gmail.com>
2019-02-11 09:20:39 -0700

    added profile image column to users table

--- db/migrations/20190205121954_users.js
+++ db/migrations/20190205121954_users.js
@@ -4,6 +4,7 @@ exports.up = function(knex) {
     table.increments('user_id').notNull().primary();
     table.string('display_name').notNull();
     table.string('account_type');
+    table.string('image_url');
     table.string('email').unique().notNull();
     table.string('hash').notNull();
     table.timestamps();
