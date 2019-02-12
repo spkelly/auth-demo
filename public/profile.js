b62e35aeed7428e53611149b8d9417e5ada8dd72
Sean Kelly <spkelly18@gmail.com>
2019-02-12 11:03:32 -0700

    styled front-end and added ajax request to profile to fetch all users

---
+++ public/profile.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  let _members = $('.members');
+  console.log(_members);
+  $.ajax({
+    method:'GET',
+    url:'/users/'
+  })
+  .done((data)=>{
+    console.log('data: ',data);
+    data.forEach(member => {
+      let memberDiv = document.createElement('div');
+      let memberName = document.createElement('h4');
+      let memberPic = document.createElement('div');
+      $(memberDiv).addClass('member card');
+      $(memberPic).addClass('member__pic');
+
+      $(memberName).addClass('member__name').text(member.display_name)
+      $(memberDiv).append(memberPic,memberName);
+      $(_members).append(memberDiv);
+    });
+  })
+  .fail((err)=>{
+    console.log('err')
+  })
+})
